ba004711fdbc5ce1d434b2b033a0c5027ee4f4ca
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-03-20 14:55:51 -0400

    Add phone details code

--- src/app/phone-details/phone-details.component.ts
+++ src/app/phone-details/phone-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { PhoneService } from '../phone.service';
 
 @Component({
   selector: 'app-phone-details',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhoneDetailsComponent implements OnInit {
 
-  constructor() { }
+  phone: Object;
+
+  constructor(
+    private myRoute: ActivatedRoute,
+    private myPhoneService: PhoneService
+  ) { }
 
   ngOnInit() {
+    this.myRoute.params.subscribe((params) => {
+      this.getPhoneDetails(params['id']);
+    });
+  }
+
+  getPhoneDetails(id) {
+    this.myPhoneService.get(id)
+      .then((thePhoneDetails) => {
+        this.phone = thePhoneDetails;
+      })
+      .catch((err) => {
+        console.log('ERROR', err);
+      });
   }
 
 }
